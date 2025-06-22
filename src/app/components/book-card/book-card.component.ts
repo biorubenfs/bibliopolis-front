695da480c9d80455133861df6a2d7f97197274d9
biorubenfs <biorubenfs@hotmail.com>
2025-06-23 00:54:09 +0200

    wip: add basic book card and books list components

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Book } from '../../interfaces/api.interfaces';
 
 @Component({
   selector: 'book-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './book-card.component.html',
 })
 export class BookCardComponent {
-
+  book = input.required<Book>()
 }
--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Book } from '../../interfaces/api.interfaces';
+import { BookCardComponent } from '../book-card/book-card.component';
 
 @Component({
   selector: 'books-list',
-  imports: [],
+  imports: [BookCardComponent],
   templateUrl: './books-list.component.html',
 })
 export class BooksListComponent {
-
+  books = input.required<Array<Book>>()
 }
