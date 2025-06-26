36c9d6a141db46da34c8264c927b2c5b2c6d0fae
biorubenfs <biorubenfs@hotmail.com>
2025-06-26 16:57:27 +0200

    wip: add bootstrap

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideMenuHeaderComponent } from "./side-menu-header/side-menu-header.component";
 import { SideMenuOptionsComponent } from "./side-menu-options/side-menu-options.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
+  imports: [RouterLink],
   templateUrl: './side-menu.component.html',
 })
 export class SideMenuComponent {
