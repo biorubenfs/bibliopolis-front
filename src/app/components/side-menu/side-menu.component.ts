f13f0a09777f5c26a5a7b1c053d1a4629206b4d7
biorubenfs <biorubenfs@hotmail.com>
2025-06-30 00:23:28 +0200

    wip: remove daisy ui components

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { SideMenuHeaderComponent } from "./side-menu-header/side-menu-header.component";
-import { SideMenuOptionsComponent } from "./side-menu-options/side-menu-options.component";
 import { RouterLink } from '@angular/router';
 
 @Component({
